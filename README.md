6b4c40740157b0be5fe2a39a81db8ad1d90c6b60
io42630 <ivan@olexyn.com>
2023-12-09 22:35:01 +0100

    + push working proto

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
-# POT SCRAPE
+# POT-SCRAPE
 
-* The goal here is to download images from hotpot.ai
+## Goal
+
+* **Download images from hotpot.ai**
+* It worked on 2023-12-03 with ~90% of ~1400 all images being downloaded
+  * some issues arose with images that were not available on the aws server
+
+# Usage
+
+  * Fill the config gaps marked by `TODO_CONFIG`.
+  * Run.
+
+
+## What it does
+
+* Login
+* Go to the _art-gallery_ and read all `artId`
+* For each `artid` 
+  * Check if the image already `exists()` in the download folder
+    * If not, download the image
+* Return to the _art-gallery_
+* For each `artid`
+  * Check if the image already `exists()` in the download folder
+    * If yes, mark the image for deletion
+    * After marking 100 images send a DELETE request.
--- src/main/java/com/olexyn/pot/scrape/App.java
+++ src/main/java/com/olexyn/pot/scrape/App.java
@@ -1,13 +1,90 @@
 package com.olexyn.pot.scrape;
 
+import com.olexyn.propconf.PropConf;
+import com.olexyn.tabdriver.TabDriver;
 import com.olexyn.tabdriver.TabDriverBuilder;
+import org.openqa.selenium.By;
 
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static com.olexyn.tabdriver.Constants.CHECKBOX;
+import static com.olexyn.tabdriver.Constants.CLASS;
+import static com.olexyn.tabdriver.Constants.DIV;
+import static com.olexyn.tabdriver.Constants.INPUT;
+import static com.olexyn.tabdriver.Constants.LABEL;
 
 public class App {
-    public static void main(String[] args) {
-        var confPath = Path.of("/home/user/home/ws/pot-scrape/src/main/resources/tabdriver.properties");
+    public static void main(String[] args) throws InterruptedException {
+
+        var confPath = Path.of("TODO_CONFIG/pot-scrape/src/main/resources/tabdriver.properties");
+        PropConf.loadProperties("TODO_CONFIG/ws/pot-scrape/src/main/resources/config.properties");
         var td = TabDriverBuilder.build(confPath);
-        int br = 0;
+        td.get("https://hotpot.ai/login");
+        td.getByFieldValue(LABEL, CLASS, "radioButton login").click();
+        td.getByFieldValue(INPUT, CLASS, "email").sendKeys(PropConf.get("user"));
+        td.getByFieldValue(INPUT, CLASS, "password").sendKeys(PropConf.get("pwd"));
+        td.getByFieldValue(DIV, CLASS, "button submit").click();
+        Thread.sleep(2000L);
+        goToArtMaker(td);
+
+        var htmlStr = td.getPageSource();
+
+        List<String> artIdList = new ArrayList<>();
+        Pattern pattern = Pattern.compile("href=\"/s/art-generator/([^\"]+)\"");
+        Matcher matcher = pattern.matcher(htmlStr);
+
+        while (matcher.find()) {
+            artIdList.add(matcher.group(1).split("/")[0]);
+        }
+
+        for (var artId : artIdList) {
+            if (!exists(artId)) {
+                td.get("https://hotpotmedia.s3.us-east-2.amazonaws.com/" + artId + ".png");
+            }
+        }
+
+        goToArtMaker(td);
+        int count = 0;
+        for (var artId : artIdList) {
+            if (!exists(artId)) {
+                continue;
+            }
+
+            var item = td.getByFieldValue(DIV, "data-itemid", artId);
+            var check = item.findElement(By.className(CHECKBOX));
+            td.setRadio(check, true);
+            count++;
+            if (count >= 100) {
+                td.getByFieldValue(DIV, CLASS, "button delete").click();
+                count = 0;
+                Thread.sleep(5000L);
+            }
+            td.getByFieldValue(DIV, CLASS, "button delete").click();
+
+        }
+
     }
+
+    private static Path expectedPath(String artId) {
+        String expectedPath = PropConf.get("download.dir") + artId + ".png";
+        return FileSystems.getDefault().getPath(expectedPath);
+    }
+
+    private static boolean exists(String artId) {
+        return Files.exists(expectedPath(artId));
+    }
+
+    private static void goToArtMaker(TabDriver td) throws InterruptedException {
+        td.get("https://hotpot.ai/drive");
+        Thread.sleep(1000L);
+        td.getByFieldValue(DIV, "serviceid", "8").click(); // Art Maker
+        Thread.sleep(15000L);
+    }
+
 }
